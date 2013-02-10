582ac0859bcab8dc89652cc1da4a20992e2ac99d
Jakob Bak <jakob.bak@gmail.com>
2013-02-10 10:42:14 +0100

    updated keywords

--- software/lib/MMM/examples/Music/_7_spaghetti_tones/_7_spaghetti_tones.ino
+++ software/lib/MMM/examples/Music/_7_spaghetti_tones/_7_spaghetti_tones.ino
@@ -1,3 +1,6 @@
+#define NUM_OSCILLATORS 3
+#define BIT_DEPTH 12
+
 // The Music and Midi objects are automatically instantiated when the header file is included.
 // Make calls to the Music and Midi objects with "Music.function(args)" and "Midi.function(args)"
 // You still need to call Music.init() and Midi.init() in the setup() function below.
