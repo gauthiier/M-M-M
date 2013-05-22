fadc5cc783c6cd027540ea160ad057e52dcb3682
Jakob Bak <jakob.bak@gmail.com>
2013-05-22 09:36:47 +0200

    fixed _8_FM.ino Arduino sketch
    A conflict in github added some lines of text to the sketch that would
    prevent it from compiling

--- software/lib/MMM/examples/Motion/_8_FM/_8_FM.ino
+++ software/lib/MMM/examples/Motion/_8_FM/_8_FM.ino
@@ -1,11 +1,7 @@
 //FM synthes of a sort
-<<<<<<< HEAD:software/lib/MMM/examples/Motion/_8_FM/_8_FM.ino
-//hang on! (it's unstable and wants to limit cycle)
-=======
 //hang on!  it's unstable and wants to limit cycle
 
 #define BIT_DEPTH 8 // gives us 16 Waveforms
->>>>>>> getting FM:software/apps/Modeling/FM/FM.ino
 
 #include <Music.h>
 
